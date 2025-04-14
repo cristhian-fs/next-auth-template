7ecb16c6164f9a8c0d4f14f0f06d596a548b6019
Cristhian <cristhianfernandolp@gmail.com>
2025-04-13 22:04:12 -0300

    chore: add initial styles and content to root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,24 @@
+import { LoginButton } from "@/components/auth/login-button";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <>
-    <h1 className="font-semibold text-3xl">
-      Hello
-    </h1>
-    <Button type="button" variant={"outline"} size={"default"}>
-      Button
-    </Button>
-    </>
-    
+    <main className="flex h-full flex-col items-center justify-center">
+      <div className="space-y-6 text-center">
+        <h1 className="text-6xl font-semibold text-white drop-shadow-md">
+          Auth
+        </h1>
+        <p className="text-white text-lg">
+          A simple authentication service
+        </p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size={"lg"}>
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   );
 }
