b138ce6144f880de951e02c31cdba14ec6f886f3
Cristhian <cristhianfernandolp@gmail.com>
2025-04-14 12:31:39 -0300

    feat: export default values for routes

---
+++ routes.ts
@@ -0,0 +1,13 @@
+export const PUBLIC_ROUTES = [
+  "/"
+]
+
+export const AUTH_ROUTES = [
+  "/auth/login",
+  "/auth/register"
+]
+
+export const API_ROUTES_PREFIX = "/api/auth"
+
+
+export const DEFAULT_LOGIN_REDIRECT = "/settings"
